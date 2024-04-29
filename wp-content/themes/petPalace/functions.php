76bafd46b4d99e99af54b6114e2f8943313e33f2
Ceeessan <Cessan__94@hotmail.com>
2024-04-29 15:29:18 +0200

    Woocommerce

--- wp-content/themes/petPalace/functions.php
+++ wp-content/themes/petPalace/functions.php
@@ -12,10 +12,10 @@ require_once("init.php");
 //Initialize theme
 require_once(get_template_directory() . "/init.php");
 
-// function moody_studio_add_woocommerce_support() {
-// 	add_theme_support( 'woocommerce' );
-// }
-// add_action( 'after_setup_theme', 'moody_studio_add_woocommerce_support' );
+function petPalace_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'petPalace_add_woocommerce_support' );
 
 
 ?>
