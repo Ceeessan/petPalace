3e5b29fda01c633ecb2c9fd24718649868c292ff
Ceeessan <Cessan__94@hotmail.com>
2024-05-21 14:05:00 +0200

    Klarna?

--- wp-content/themes/petPalace/resources/scripts/app.js
+++ wp-content/themes/petPalace/resources/scripts/app.js
@@ -1,31 +1,11 @@
 // alert("Hello world");
 
-<<<<<<< HEAD
-//Listing:
-document.addEventListener('DOMContentLoaded', function () {
-    const couponInput = document.querySelector('#coupon_code');
-    const applyCouponButton = document.querySelector('button[name="apply_coupon"]');
-
-    function toggleCouponButton() {
-        if (couponInput.value.trim() === '') {
-            applyCouponButton.disabled = true;
-        } else {
-            applyCouponButton.disabled = false;
-        }
-    }
-
-    toggleCouponButton();
-
-    couponInput.addEventListener('input', toggleCouponButton);
-});
 
 //För ikonerna katt|hund|gnagare|fågel
 jQuery(document).ready(function ($) {
     $('.icon-animals').on('click', function () {
-        var category = $(this).data('category');
-        var url = ajax_variables.siteUrl + '/product-category/' + category;
+        var tag = $(this).data('tag');
+        var url = ajax_variables.siteUrl + '/product-tag/' + tag;
         window.location.href = url;
     });
 });
-=======
->>>>>>> main
