0daad2ef667cec5defea2dedb9e8e8893597826a
Annie <125252462+JunfangAnnie@users.noreply.github.com>
2024-05-21 14:05:48 +0200

    lagt till önskelisteikonen i cart

--- wp-content/themes/petPalace/woocommerce/cart/cart.php
+++ wp-content/themes/petPalace/woocommerce/cart/cart.php
@@ -47,22 +47,26 @@ do_action( 'woocommerce_before_cart' ); ?>
 					?>
 					<tr class="woocommerce-cart-form__cart-item <?php echo esc_attr( apply_filters( 'woocommerce_cart_item_class', 'cart_item', $cart_item, $cart_item_key ) ); ?>">
 
-						<td class="product-remove">
-							<?php
-								echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-									'woocommerce_cart_item_remove_link',
-									sprintf(
-										'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
-										esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
-										/* translators: %s is the product name */
-										esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
-										esc_attr( $product_id ),
-										esc_attr( $_product->get_sku() )
-									),
-									$cart_item_key
-								);
-							?>
-						</td>
+					<td class="product-remove">
+						<?php
+						echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+							'woocommerce_cart_item_remove_link',
+							sprintf(
+								'<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+								esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
+            					/* translators: %s is the product name */
+            					esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $product_name ) ) ),
+            					esc_attr( $product_id ),
+            					esc_attr( $_product->get_sku() )
+        					),
+        					$cart_item_key
+    						);
+    					?>
+    					<div class="wishlist-icon">
+        					<img src="<?php echo esc_url( get_template_directory_uri() . '/resources/images/wishlist.svg' ); ?>" alt="Custom Icon" />
+    					</div>
+					</td>
+
 
 						<td class="product-thumbnail">
 						<?php
